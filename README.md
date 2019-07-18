d45c77bc296aeca517e2455bf0d677cd1f6fb775
filipenevola <filipemkv@gmail.com>
2019-07-18 12:24:29 +0200

    adds --settings option

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 Inspired by [galaxy-autoscale](https://github.com/jehartzog/galaxy-autoscale) this is a node 
 program to monitor and auto-scale Meteor Galaxy
+
+## Settings
+
+```json
+{
+  "appName": "your app host",
+  "username": "your Galaxy username",
+  "password": "your Galaxy password",
+  "slackWebhook": "your Slack webhook URL",
+  "persistentStorage": "full path to where we want to storage scrapped info",
+  "infoRules": {
+    "send": false
+  },
+  "alertRules": {
+    "maxInContainers": {
+      "cpu": 1,
+      "memory": 10,
+      "clients": 5
+    }
+  },
+  "minimumStats": 5,
+  "puppeteer": {
+    "headless": false
+  }
+}
+
+```
--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import '@babel/polyfill';
+import fs from 'fs-extra';
+import yargs from 'yargs';
 import 'dotenv/config';
 import { sync } from './sync';
 
 const main = async () => {
-  const options = {
-    appName: process.env.APP_NAME,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    slackWebhook: process.env.SLACK_WEBHOOK,
-    persistentStorage: process.env.PERSISTENT_STORAGE,
-    scalingRules: {
-      containersMin: 1,
-      containersMax: 3,
-      connectionsPerContainerMax: 80,
-      connectionsPerContainerMin: 40,
-    },
-    infoRules: {
-      send: false,
-    },
-    alertRules: {
-      maxInContainers: {
-        cpu: 1,
-        memory: 10,
-        clients: 5,
-      },
-    },
-    minimumStats: 5,
-    puppeteer: {
-      headless: process.env.HEADLESS == 'true',
-    },
-  };
-  const storage = await sync(options);
-  console.log('storage', storage);
+  const argv = yargs
+    .usage('Usage: $0 --settings settings.json')
+    .demandOption(['settings'])
+    .option('settings', {
+      description: 'path to JSON settings',
+      type: 'string',
+    })
+    .help()
+    .alias('help', 'h').argv;
+  const options = fs.readJsonSync(argv.settings);
+  await sync(options);
 };
 
 main().catch(e => console.error(e));
