4892cbc2a4cbcc2a8c55e045e1b8b5059b710c1a
Gywem <igcogi@gmail.com>
2019-11-15 11:22:06 +0100

    fix a bug that produced the job to not run scale actions

--- src/autoscale.js
+++ src/autoscale.js
@@ -118,15 +118,19 @@ const ALL_CHECKS = [
   },
 ];
 
-function checkResultToText(scaledSuccessChecks) {
-  return `${scaledSuccessChecks
-    .map(
-      c =>
-        `${c.metricField} ${c.metricValue} is ${
-          c.greaterThan ? 'greater than' : 'less than'
-        } ${c.whenField} ${c.whenValue}`
-    )
-    .join(', ')}`;
+function checkResultToText(scaledSuccessChecksOrNull) {
+  return (
+    (scaledSuccessChecksOrNull &&
+      `${scaledSuccessChecksOrNull
+        .map(
+          c =>
+            `${c.metricField} ${c.metricValue} is ${
+              c.greaterThan ? 'greater than' : 'less than'
+            } ${c.whenField} ${c.whenValue}`
+        )
+        .join(', ')}`) ||
+    ''
+  );
 }
 
 const checkAction = (action, rules, metricsParam, { andMode = true } = {}) => {
